7fca01430ab6c34a1896b7b02dca7198d58adb43
Anna <eltyshevaanna2006@yandex.ru>
2026-03-10 18:01:29 +0500

    Проверка

--- security.permaviat.ru/ajax/recovery.php
+++ security.permaviat.ru/ajax/recovery.php
@@ -1,46 +1,195 @@
 <?php
-	session_start();
-	include("../settings/connect_datebase.php");
-	
-	$login = $_POST['login'];
-	
-	// ищем пользователя
-	$query_user = $mysqli->query("SELECT * FROM `users` WHERE `login`='".$login."';");
-	
-	$id = -1;
-	if($user_read = $query_user->fetch_row()) {
-		// создаём новый пароль
-		$id = $user_read[0];
-	}
-	
-	function PasswordGeneration() {
-		// создаём пароль
-		$chars="qazxswedcvfrtgbnhyujmkiolp1234567890QAZXSWEDCVFRTGBNHYUJMKIOLP"; // матрица
-		$max=10; // количество
-		$size=StrLen($chars)-1; // Определяем количество символов в $chars
-		$password="";
-		
-		while($max--) {
-			$password.=$chars[rand(0,$size)];
-		}
-		
-		return $password;
-	}
-	
-	if($id != 0) {
-		//обновляем пароль
-		$password = PasswordGeneration();;
-		// проверяем не используется ли пароль 
-		$query_password = $mysqli->query("SELECT * FROM `users` WHERE `password`= '".md5($password)."';");
-		while($password_read = $query_password->fetch_row()) {
-			// создаём новый пароль
-			$password = PasswordGeneration();
-		}
-		// обновляем пароль
-		$mysqli->query("UPDATE `users` SET `password`='".md5($password)."' WHERE `login` = '".$login."'");
-		// отсылаем на почту
-		//mail($login, 'Безопасность web-приложений КГАПОУ "Авиатехникум"', "Ваш пароль был только что изменён. Новый пароль: ".$password);
-	}
-	
-	echo $id;
+session_start();
+include("../settings/connect_datebase.php");
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$login = $_POST['login'] ?? '';
+
+if (empty($login)) {
+    echo -1;
+    exit;
+}
+
+// Логирование
+$log_file = dirname(__DIR__) . '/recovery_log.txt';
+file_put_contents($log_file, date('Y-m-d H:i:s') . " - Начало восстановления для: $login\n", FILE_APPEND);
+
+$login_escaped = $mysqli->real_escape_string($login);
+
+// Ищем пользователя
+$query_user = $mysqli->query("
+    SELECT * FROM `users` 
+    WHERE `login` = '$login_escaped' 
+       OR `email` = '$login_escaped'
+");
+
+$id = -1;
+$user_data = null;
+
+if ($query_user && $query_user->num_rows > 0) {
+    $user_data = $query_user->fetch_assoc();
+    $id = $user_data['id'];
+    file_put_contents($log_file, date('Y-m-d H:i:s') . " - Найден пользователь ID: $id\n", FILE_APPEND);
+} else {
+    file_put_contents($log_file, date('Y-m-d H:i:s') . " - Пользователь не найден\n", FILE_APPEND);
+    echo -1;
+    exit;
+}
+
+function PasswordGeneration() {
+    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    $length = 10;
+    $password = "";
+    for ($i = 0; $i < $length; $i++) {
+        $password .= $chars[rand(0, strlen($chars) - 1)];
+    }
+    return $password;
+}
+
+function sendSMTP($to, $new_password, $login) {
+    $log_file = dirname(__DIR__) . '/recovery_log.txt';
+    
+    // Данные для SMTP Яндекса
+    $smtp_host = 'smtp.yandex.ru';
+    $smtp_port = 587;
+    $smtp_user = 'eltyshevaanna2006@yandex.ru';
+    $smtp_pass = 'wcvpzyjlenbhtdet';
+    $from_email = 'eltyshevaanna2006@yandex.ru';
+    $from_name = 'Система безопасности';
+    
+    $newline = "\r\n";
+    
+    try {
+        $socket = fsockopen($smtp_host, $smtp_port, $errno, $errstr, 30);
+        
+        if (!$socket) {
+            file_put_contents($log_file, date('Y-m-d H:i:s') . " | Ошибка подключения SMTP: $errstr\n", FILE_APPEND);
+            return false;
+        }
+        
+        stream_set_timeout($socket, 30);
+        
+        
+        $response = fgets($socket, 512);
+        
+        
+        fputs($socket, "EHLO localhost" . $newline);
+        while ($line = fgets($socket, 512)) {
+            if (substr($line, 3, 1) == ' ') break;
+        }
+        
+        
+        fputs($socket, "STARTTLS" . $newline);
+        $response = fgets($socket, 512);
+        
+        
+        stream_socket_enable_crypto($socket, true, STREAM_CRYPTO_METHOD_TLS_CLIENT);
+        
+        
+        fputs($socket, "EHLO localhost" . $newline);
+        while ($line = fgets($socket, 512)) {
+            if (substr($line, 3, 1) == ' ') break;
+        }
+        
+        
+        fputs($socket, "AUTH LOGIN" . $newline);
+        fgets($socket, 512);
+        
+        fputs($socket, base64_encode($smtp_user) . $newline);
+        fgets($socket, 512);
+        
+        fputs($socket, base64_encode($smtp_pass) . $newline);
+        $response = fgets($socket, 512);
+        
+        
+        fputs($socket, "MAIL FROM: <$from_email>" . $newline);
+        fgets($socket, 512);
+        
+        
+        fputs($socket, "RCPT TO: <$to>" . $newline);
+        fgets($socket, 512);
+        
+        
+        fputs($socket, "DATA" . $newline);
+        fgets($socket, 512);
+        
+        // Формируем письмо
+        $subject = "Восстановление пароля";
+        $message = "Здравствуйте, $login!\r\n\r\n";
+        $message .= "Ваш новый пароль для входа в систему: $new_password\r\n\r\n";
+        $message .= "Войти в систему: http://" . $_SERVER['HTTP_HOST'] . "/login.php\r\n\r\n";
+        $message .= "После входа рекомендуем сменить пароль в личном кабинете.\r\n\r\n";
+        $message .= "Если вы не запрашивали восстановление пароля, проигнорируйте это письмо.\r\n\r\n";
+        $message .= "---\r\n";
+        $message .= "Система безопасности веб-приложений\r\n";
+        $message .= "Пермский авиационный техникум им. А. Д. Швецова\r\n";
+        
+        $headers = "From: $from_name <$from_email>" . $newline;
+        $headers .= "To: $to" . $newline;
+        $headers .= "Reply-To: $from_email" . $newline;
+        $headers .= "Subject: =?UTF-8?B?" . base64_encode($subject) . "?=" . $newline;
+        $headers .= "Content-Type: text/plain; charset=utf-8" . $newline;
+        $headers .= "MIME-Version: 1.0" . $newline;
+        
+        // Отправляем
+        fputs($socket, $headers . $newline . $message . $newline . "." . $newline);
+        $response = fgets($socket, 512);
+        
+        
+        fputs($socket, "QUIT" . $newline);
+        fclose($socket);
+        
+        file_put_contents($log_file, date('Y-m-d H:i:s') . " | Письмо успешно отправлено на $to\n", FILE_APPEND);
+        return true;
+        
+    } catch (Exception $e) {
+        if (isset($socket) && $socket) {
+            fclose($socket);
+        }
+        file_put_contents($log_file, date('Y-m-d H:i:s') . " | Ошибка: " . $e->getMessage() . "\n", FILE_APPEND);
+        return false;
+    }
+}
+
+if ($id != -1) {
+    // Создаем новый пароль
+    $new_password = PasswordGeneration();
+    $hashed_password = password_hash($new_password, PASSWORD_DEFAULT);
+    
+    file_put_contents($log_file, date('Y-m-d H:i:s') . " - Новый пароль: $new_password\n", FILE_APPEND);
+    
+    // Обновляем пароль в базе
+    $update_query = "UPDATE `users` SET `password` = '$hashed_password' WHERE `id` = $id";
+    
+    if ($mysqli->query($update_query)) {
+        file_put_contents($log_file, date('Y-m-d H:i:s') . " - Пароль обновлен в БД\n", FILE_APPEND);
+        
+        // Определяем email для отправки
+        $email_to = $user_data['email'] ?? '';
+        
+        if (empty($email_to)) {
+            // Если email пустой, используем логин, если он похож на email
+            if (strpos($user_data['login'], '@') !== false) {
+                $email_to = $user_data['login'];
+            }
+        }
+        
+        if (!empty($email_to)) {
+            // Отправляем через SMTP
+            $sent = sendSMTP($email_to, $new_password, $user_data['login']);
+            
+            if ($sent) {
+                file_put_contents($log_file, date('Y-m-d H:i:s') . " - Письмо отправлено на $email_to\n", FILE_APPEND);
+            } else {
+                file_put_contents($log_file, date('Y-m-d H:i:s') . " - Ошибка отправки письма\n", FILE_APPEND);
+            }
+        } else {
+            file_put_contents($log_file, date('Y-m-d H:i:s') . " - Нет email для отправки\n", FILE_APPEND);
+        }
+    }
+}
+
+echo $id;
 ?>
--- security.permaviat.ru/recovery.php
+++ security.permaviat.ru/recovery.php
@@ -1,133 +1,234 @@
 <?php
-	session_start();
-	if (isset($_SESSION['user'])) {
-		if($_SESSION['user'] != -1) {
-			include("./settings/connect_datebase.php");
-			
-			$user_query = $mysqli->query("SELECT * FROM `users` WHERE `id` = ".$_SESSION['user']);
-			while($user_read = $user_query->fetch_row()) {
-				if($user_read[3] == 0) header("Location: user.php");
-				else if($user_read[3] == 1) header("Location: admin.php");
-			}
-		}
- 	}
+session_start();
+if (isset($_SESSION['user'])) {
+    if($_SESSION['user'] != -1) {
+        include("./settings/connect_datebase.php");
+        
+        $user_query = $mysqli->query("SELECT * FROM `users` WHERE `id` = ".$_SESSION['user']);
+        while($user_read = $user_query->fetch_row()) {
+            if($user_read[3] == 0) header("Location: user.php");
+            else if($user_read[3] == 1) header("Location: admin.php");
+        }
+    }
+}
 ?>
 <!DOCTYPE HTML>
 <html>
-	<head> 
-		<script src="https://code.jquery.com/jquery-1.8.3.js"></script>
-		<meta charset="utf-8">
-		<title> Восстановление пароля </title>
-		
-		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-		<link rel="stylesheet" href="style.css">
-	</head>
-	<body>
-		<div class="top-menu">
-			<a href=# class = "singin"><img src = "img/ic-login.png"/></a>
-		
-			<a href=#><img src = "img/logo1.png"/></a>
-			<div class="name">
-				<a href="index.php">
-					<div class="subname">Электронная приемная комиссия</div>
-					Пермского авиационного техникума им. А. Д. Швецова
-				</a>
-			</div>
-		</div>
-		<div class="space"> </div>
-		<div class="main">
-			<div class="content">
-				<div class="input-error">
-					<img src="img/ic-close.png" class="close" onclick="DisableError()"/>
-					<img src = "img/ic-error.png"/>
-					Непредвиденная ошибка.
-					<div class="message">Указанный вами адрес электронной почты не существует в системе, проверьте правильность ввода данных.</div>
-				</div>
-			
-				<div class="success" style="display: none;">
-					<img src = "img/ic_success.png">
-					<div class = "name">Успешно!</div>
-					<div class = "description">
-						На указанный вами адрес будет отправлено письмо с новым паролем.
-					</div>
-				</div>
-			
-				<div class = "login">
-					<div class="name">Восстановление пароля</div>
-				
-					<div class = "sub-name">Почта (логин):</div>
-					<div style="font-size: 12px; margin-bottom: 10px;">На указанную вами почту будет выслан новый пароль, для входа в систему.</div>
-					<input name="_login" type="text" placeholder="E-mail@mail.ru"/>
-					
-					<input type="button" class="button" value="Отправить" onclick="LogIn()" style="margin-top: 0px;"/>
-					<img src = "img/loading.gif" class="loading" style="margin-top: 0px;"/>
-				</div>
-				
-				<div class="footer">
-					© КГАПОУ "Авиатехникум", 2020
-					<a href=#>Конфиденциальность</a>
-					<a href=#>Условия</a>
-				</div>
-			</div>
-		</div>
-		
-		<script>
-			var errorWindow = document.getElementsByClassName("input-error")[0];
-			var loading = document.getElementsByClassName("loading")[0];
-			var button = document.getElementsByClassName("button")[0];
-			
-			errorWindow.style.display = "none";
-		
-			function DisableError() {
-				errorWindow.style.display = "none";
-			}
-			
-			function EnableError() {
-				errorWindow.style.display = "block";
-			}
-			
-			function LogIn() {
-				var _login = document.getElementsByName("_login")[0].value;
-				loading.style.display = "block";
-				button.className = "button_diactive";
-				
-				var data = new FormData();
-				data.append("login", _login);
-				
-				// AJAX запрос
-				$.ajax({
-					url         : 'ajax/recovery.php',
-					type        : 'POST', // важно!
-					data        : data,
-					cache       : false,
-					dataType    : 'html',
-					// отключаем обработку передаваемых данных, пусть передаются как есть
-					processData : false,
-					// отключаем установку заголовка типа запроса. Так jQuery скажет серверу что это строковой запрос
-					contentType : false, 
-					// функция успешного ответа сервера
-					success: function (_data) {
-						
-						if(_data == -1) {
-							EnableError();
-							loading.style.display = "none";
-							button.className = "button";
-						} else {
-							console.log("Пароль изменён, ID абитуриента: " +_data);
-							document.getElementsByClassName('success')[0].style.display = "block";
-							document.getElementsByClassName('description')[0].innerHTML = "На указанный вами адрес <b>"+_login+"</b> будет отправлено письмо с новым паролем.";
-							
-							document.getElementsByClassName('login')[0].style.display = "none";
-						}
-					},
-					// функция ошибки
-					error: function( ){
-						console.log('Системная ошибка!');
-						loading.style.display = "none";
-						button.className = "button";
-					}
-				});
-			}
-		</script>
-	</body>
+<head> 
+    <script src="https://code.jquery.com/jquery-1.8.3.js"></script>
+    <meta charset="utf-8">
+    <title>Восстановление пароля</title>
+    
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
+    <style>
+        
+        .input-error {
+            background-color: #ff9797;
+            box-shadow: 0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.12), 0 1px 5px 0 rgba(0,0,0,.2);
+            margin-bottom: 20px;
+            padding: 20px;
+            overflow: hidden;
+            font-size: 18px;
+            color: #000;
+            font-weight: bold;
+            display: none;
+            position: relative;
+            border-left: 5px solid #dc3545;
+        }
+        
+        .close {
+            position: absolute;
+            top: 10px;
+            right: 15px;
+            cursor: pointer;
+            font-size: 20px;
+            font-weight: bold;
+            color: #333;
+            background: none;
+            border: none;
+            padding: 0 5px;
+        }
+        
+        .close:hover {
+            color: #000;
+            background-color: rgba(0,0,0,0.1);
+        }
+        
+        .message {
+            font-size: 14px;
+            font-weight: normal;
+            margin-top: 10px;
+        }
+        
+        .success {
+            background-color: #d4edda;
+            color: #155724;
+            border: 1px solid #c3e6cb;
+            border-radius: 5px;
+            padding: 20px;
+            margin-bottom: 20px;
+            display: none;
+            border-left: 5px solid #28a745;
+        }
+        
+        .success .name {
+            font-size: 20px;
+            font-weight: bold;
+            margin-bottom: 10px;
+        }
+        
+        .loading {
+            display: none;
+            margin-top: 10px;
+            text-align: center;
+            color: #666;
+        }
+        
+      
+        .top-menu {
+            width: 100%;
+            height: 100px;
+            background-color: #FFFFFF;
+            border-bottom: 1px solid #DADCE0;
+            position: fixed;
+        }
+        
+        .top-menu .logo-placeholder {
+            width: 50px;
+            height: 50px;
+            margin-left: 100px;
+            margin-top: 25px;
+            float: left;
+            margin-right: 25px;
+            background-color: #1A73E8;
+            border-radius: 5px;
+        }
+        
+        .top-menu .name {
+            padding-top: 20px;
+        }
+        
+        .top-menu .name a {
+            font-size: 15.5px;
+            color: #5F6368;
+            text-decoration: none;
+        }
+        
+        .top-menu .name a .subname {
+            font-size: 23px;
+            color: #5F6368;
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+    <div class="top-menu">
+        <div class="logo-placeholder"></div>
+        <div class="name">
+            <a href="index.php">
+                <div class="subname">Электронная приемная комиссия</div>
+                Пермского авиационного техникума им. А. Д. Швецова
+            </a>
+        </div>
+    </div>
+    <div class="space"></div>
+    <div class="main">
+        <div class="content">
+            <div class="input-error">
+                <span class="close" onclick="DisableError()">✕</span>
+                <strong>Ошибка!</strong>
+                <div class="message">Указанный вами адрес электронной почты не существует в системе. Проверьте правильность ввода данных.</div>
+            </div>
+        
+            <div class="success" style="display: none;">
+                <div class="name">Успешно!</div>
+                <div class="description">
+                    На указанный вами адрес будет отправлено письмо с новым паролем.
+                </div>
+            </div>
+        
+            <div class="login">
+                <div class="name">Восстановление пароля</div>
+            
+                <div class="sub-name">Email (логин):</div>
+                <div style="font-size: 12px; margin-bottom: 10px;">На указанную вами почту будет выслан новый пароль для входа в систему.</div>
+                <input name="_login" type="text" placeholder="example@mail.ru"/>
+                
+                <div style="overflow: hidden; margin-top: 20px;">
+                    <input type="button" class="button" value="Отправить" onclick="LogIn()" style="float: right; margin: 0;"/>
+                    <div class="loading" style="float: right; margin-right: 20px; margin-top: 8px;">Отправка...</div>
+                </div>
+            </div>
+            
+            <div class="footer">
+                © КГАПОУ "Авиатехникум", 2020
+                <a href="#">Конфиденциальность</a>
+                <a href="#">Условия</a>
+            </div>
+        </div>
+    </div>
+    
+    <script>
+        var errorWindow = document.getElementsByClassName("input-error")[0];
+        var loading = document.getElementsByClassName("loading")[0];
+        var button = document.getElementsByClassName("button")[0];
+        
+        errorWindow.style.display = "none";
+    
+        function DisableError() {
+            errorWindow.style.display = "none";
+        }
+        
+        function EnableError() {
+            errorWindow.style.display = "block";
+        }
+        
+        function LogIn() {
+            var _login = document.getElementsByName("_login")[0].value;
+            
+            if(_login.trim() === "") {
+                alert("Введите email");
+                return;
+            }
+            
+            loading.style.display = "block";
+            button.disabled = true;
+            button.value = "Отправка...";
+            
+            var data = new FormData();
+            data.append("login", _login);
+            
+            $.ajax({
+                url: 'ajax/recovery.php',
+                type: 'POST',
+                data: data,
+                cache: false,
+                dataType: 'html',
+                processData: false,
+                contentType: false,
+                success: function(_data) {
+                    if(_data == -1) {
+                        EnableError();
+                        loading.style.display = "none";
+                        button.disabled = false;
+                        button.value = "Отправить";
+                    } else {
+                        document.getElementsByClassName('success')[0].style.display = "block";
+                        document.getElementsByClassName('description')[0].innerHTML = "На указанный вами адрес <b>"+_login+"</b> будет отправлено письмо с новым паролем.";
+                        
+                        document.getElementsByClassName('login')[0].style.display = "none";
+                        loading.style.display = "none";
+                    }
+                },
+                error: function() {
+                    alert("Системная ошибка. Попробуйте позже.");
+                    loading.style.display = "none";
+                    button.disabled = false;
+                    button.value = "Отправить";
+                }
+            });
+        }
+    </script>
+</body>
 </html>
